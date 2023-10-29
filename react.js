1f879e2ec05d46775bcd5330d06fb70a9d57f4c1
Agnidipa <agnidipam@gmail.com>
2023-10-29 22:17:41 +0530

    first commit

---
+++ react.js
@@ -0,0 +1,9 @@
+
+
+function BulbOn() {
+    <button id="on" class="bulb_btn" onclick="document.getElementById('myImage').src='pic_bulbon.gif'">ON</button>
+}
+
+function BulbOff () {
+    <button id="off" class="bulb_btn" onclick="document.getElementById('myImage').src='pic_bulboff.gif'">OFF</button>
+}
