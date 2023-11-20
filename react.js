2a32ac32bf9840c491df7fdf09d324452a434550
Agnidipa <agnidipam@gmail.com>
2023-11-20 21:43:42 +0530

    ami sob delete korlam

--- react.js
+++
@@ -1,9 +0,0 @@
-
-
-function BulbOn() {
-    <button id="on" class="bulb_btn" onclick="document.getElementById('myImage').src='pic_bulbon.gif'">ON</button>
-}
-
-function BulbOff () {
-    <button id="off" class="bulb_btn" onclick="document.getElementById('myImage').src='pic_bulboff.gif'">OFF</button>
-}
